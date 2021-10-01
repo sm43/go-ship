7b0300b75e20b20f4cd42e77d41d564109a6b737
Shivam Mukhade <smukhade@redhat.com>
2021-10-01 13:47:56 +0530

    case6

---
+++ case-6.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var mut sync.Mutex
+
+// using locks
+
+func main() {
+
+	alice := 10000
+	bob := 10000
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			mut.Lock()
+			alice -= 1
+
+			// without this incre and decre happens at same time
+			// so the total would be correct
+
+			// at some given time, the total will not be correct
+			// but after completing all routine the total will be correct
+
+			// mut.Unlock()
+			// mut.Lock()
+			bob += 1
+			mut.Unlock()
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			mut.Lock()
+			bob -= 1
+			// mut.Unlock()
+			// mut.Lock()
+			alice += 1
+			mut.Unlock()
+		}
+	}()
+
+	start := time.Now()
+	for time.Since(start) < 1*time.Second {
+		mut.Lock()
+		if alice+bob != 20000 {
+			fmt.Printf("\n observed violation, alice = %v, bob = %v, sum = %v \n", alice, bob, alice+bob)
+		}
+		mut.Unlock()
+	}
+
+}
