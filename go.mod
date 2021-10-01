210fe884cf6470d4876cb92579c61247d2638c11
Shivam Mukhade <smukhade@redhat.com>
2021-10-01 11:46:10 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sm43/distributed
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "sync"
+
+func main() {
+	var a  string
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func ()  {
+		a = "hello world"
+		wg.Done()	
+	}()
+	wg.Wait()
+	print(a)
+}
